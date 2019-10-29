3b611094b68f678e0a6b3c56c93ae3ed146ff588
agzsoftsi <agzsoftsi@gmail.com>
2019-10-29 22:31:19 +0000

    fix int and decimal functions again

--- _printf_dec.c
+++ _printf_dec.c
@@ -10,48 +10,41 @@
  * -------------------------------------------------------------
  * Authors - Carlos Garcia - Orlando Gomez - Cohort 10 - Cali
  * Project Date - 25/10/2019 - 29/10/2019
- * -------------------------------------------------------------
+ * ------------------------------------------------------------
  **/
 int pd(va_list arg)
 {
-	int n, num, ld;
-	int dig;
-	int exp, con;
-	int val = 1;
+	int con = 0, n = va_arg(arg, int);
+	int num, dig, i, lon;
 
-	n = va_arg(arg, int);
+	lon = n;
+	printf("%d", lon);
 	if (n < 0)
-		val = -1;
-	ld = n % 10;
-	con = 1;
-	exp = 1;
-	n = n / 10;
-	num = n;
-	if (val < 0)
 	{
+		con++;
+		lon = lon * -1;
 		_putchar('-');
-		num = -num;
-		n = -n;
-		ld = -ld;
-		++con;
 	}
-	if (num > 0)
+
+	if (lon == 0)
 	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			dig = num / exp;
-			_putchar(dig + '0');
-			num = num - (dig * exp);
-			exp = exp / 10;
-			++con;
-		}
+		con++;
+		_putchar('0');
+		return (con);
 	}
-	_putchar(ld + '0');
+
+	i = 1;
+	while ((lon / i) > 9)
+	{
+		i = i * 10;
+	}
+	while (i > 0)
+	{
+		num = lon / i;
+		dig = num % 10;
+		con++;
+		_putchar(dig + '0');
+		i = (i / 10);
+	}
 	return (con);
 }
